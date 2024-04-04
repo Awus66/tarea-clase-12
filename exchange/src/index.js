eb57249a69e04ff567bdb30753196fb242b431da
Awus66 <conde.agustin66@gmail.com>
2024-04-04 00:37:43 -0300

    Modifica función para cargar símbolos

--- exchange/src/index.js
+++ exchange/src/index.js
@@ -1,3 +1,10 @@
+const $submitButton = document.querySelector('#submit-button');
+const $from = document.querySelector('#from');
+const $to = document.querySelector('#to');
+const $amount = document.querySelector('#amount');
+const $startDate = document.querySelector('#start-date');
+const $endDate = document.querySelector('#end-date');
+
 loadSymbols();
 const $symbol = document.querySelector('#symbol');
 
@@ -7,10 +14,19 @@ function loadSymbols () {
     .then(response => response.json())
     .then(response => {
         Object.keys(response).forEach(symbol =>{
-            const $newSymbol = document.createElement('option');
-            $newSymbol.value = symbol;
-            $newSymbol.innerText = symbol;
-            $symbol.appendChild($newSymbol);
+            const $newFromSymbol = document.createElement('option');
+            $newFromSymbol.value = symbol;
+            $newFromSymbol.innerText = symbol;
+            $from.appendChild($newFromSymbol);
+
+            if (symbol === 'EUR'){
+                $newFromSymbol.selected = true;
+            }
+
+            const $newToSymbol = document.createElement('option');
+            $newToSymbol.value = symbol;
+            $newToSymbol.innerText = symbol;
+            $to.appendChild($newToSymbol);
         });
     });
 }
