90868ed9b7b4caa4fb9b8f3a0dcb812c154f967b
Awus66 <conde.agustin66@gmail.com>
2024-05-09 09:11:42 -0300

    Implementa función para asignar los pokemon por pagina

--- pokedex/src/index.js
+++ pokedex/src/index.js
@@ -10,3 +10,39 @@ const $totalPages = document.querySelector('#total-pages');
 const $pokemons = document.querySelector('#pokemons');
 const $closeButton = document.querySelector('.close');
 let pokemonCount = 0;
+
+setPagesAmount();
+
+
+function setPagesAmount() {
+    fetch(URL)
+    .then(response => response.json())
+    .then(response => {
+        const fetchPromises = [];
+
+        for (let i = 1; i <= response["count"]; i++) {
+            fetchPromises.push(
+                fetch(`${URL}${i}`)
+                .then(response => response.json())
+                .then(pokemon => {
+                    if(pokemon){
+                        pokemonCount++;
+                        allPokemons.push(pokemon);
+                    }
+                })
+                .catch(error => {
+                    console.error('Error fetching Pokémon with ID:', i, error);
+                })
+            );
+        }
+
+        Promise.all(fetchPromises)
+        .then(() => {
+            const totalPages = Math.ceil(pokemonCount / POKEMONS_PER_PAGE);
+            $totalPages.value = totalPages;
+            document.querySelector('#loading-spinner').style.display = 'none';
+            showPokemons();
+        });
+    });
+}
+
