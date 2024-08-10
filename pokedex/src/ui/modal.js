18614bf1f7b0f23eec85bd4fb282df78df63065a
Awus66 <conde.agustin66@gmail.com>
2024-08-10 01:23:13 -0300

    Añade clase para pokemon
    Se implementan clases al manejar los datos de los pokemones para cumplir con la consigna de la tarea de la clase 15

--- pokedex/src/ui/modal.js
+++ pokedex/src/ui/modal.js
@@ -18,13 +18,13 @@ window.onclick = function(event) {
 
 export function showModal(pokemon) {
     $modalContent.innerHTML = `
-        <h2>${pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</h2>
-        <img src="${pokemon.sprites.front_default}" alt="${pokemon.name}" />
+        <h2>${pokemon.name}</h2>
+        <img src="${pokemon.sprite}" alt="${pokemon.name}" />
         <p>ID: ${pokemon.id}</p>
-        <p>Height: ${Number(pokemon.height)/10} m</p>
-        <p>Weight: ${Number(pokemon.weight)/10} kg</p>
-        <p>Type: ${pokemon.types.map(type => capitalize(type.type.name)).join(", ")}</p>
-        <p>Abilities: ${pokemon.abilities.map(ability => capitalize(ability.ability.name).replace(/-/g, " ")).join(", ")}</p>
+        <p>Height: ${pokemon.height/10} m</p>
+        <p>Weight: ${pokemon.weight/10} kg</p>
+        <p>Type: ${pokemon.types}</p>
+        <p>Abilities: ${pokemon.abilities}</p>
     `;
     $modal.style.display = "block";
 }
