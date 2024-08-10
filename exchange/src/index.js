b46d8d5daf28a9d96517befb17265e2c98751783
Awus66 <conde.agustin66@gmail.com>
2024-08-10 14:23:46 -0300

    Refactoriza codigo y agrega clases
    Se cambia la lógica del código y se agregan clases y métodos para cumplir con la consigna de la tarea de la clase 15

--- exchange/src/index.js
+++ exchange/src/index.js
@@ -1,11 +1,114 @@
+class Exchange{
+    constructor(from, to = null, amount = 1 , startDate = null, endDate = null){
+        this.from = from;
+        this.to = to;
+        this.amount = amount;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.data = null;
+    }
+
+
+    getFetchURL() {
+        let URL = `https://api.frankfurter.app/`;
+
         if (this.startDate) {
+            URL += `${this.startDate}..`;
+        }
+
         if (this.endDate) {
+            URL += `${this.endDate}`;
+        }
+
         if (!this.startDate && !this.endDate) {
+            URL += `latest`;
+        }
+
+        URL += `?from=${this.from}`;
+
+        if (this.from === this.to){
+            this.to = null;
+        }
         else if (this.to) {
+            URL += `&to=${this.to}`;
+        }
+
+        URL += `&amount=${this.amount}`;
+
+        return URL;
+    }
+
+    async getRates() {
+        const $data = document.querySelector('#data');
+
+        fetch(this.getFetchURL())
+        .then(response => response.json())
+        .then(response => {
+            let dataText = `Exchange Rates for ${this.amount} ${this.from} `;
+    
             if (this.to){
+                dataText = dataText + `to ${this.to} `;
+            }
+    
+            dataText = dataText + `on `
+    
+            let dataDate;
+    
             if (this.startDate){
+                dataDate = `${this.startDate}`;
                 if (this.endDate){
+                    dataDate = dataDate + ` - ${this.endDate}`;
+    
+                    if (this.startDate === this.endDate){
+                        dataDate = `${this.endDate}`;
+                    }
+                }
+                else {
+                    dataDate = dataDate + ` - ${currentTime}`;
+                }
+            }
+            else {
                 if (this.endDate){
+                    dataDate = `${this.endDate}`;
+                }
+                else {
+                    dataDate = `${currentTime}`;
+                }
+            }
+    
+            dataText = dataText + dataDate;
+            
+            $data.innerText = dataText;
+            $data.classList.add('fs-4', 'fw-bold');
+    
+            const $ratesContainer = document.createElement('span');
+            $ratesContainer.id = 'rates';
+    
+            Object.entries(response['rates']).forEach(([key, values]) =>{
+                const $newRate = document.createElement('li');
+                let ratesText;
+    
+                if (Object.entries(values).length > 0){
+                    ratesText = Object.entries(values).map(([currency, value]) => `\n${currency}: ${value}`).join('');
+                }
+                else{
+                    ratesText = values;
+                }
+    
+                $newRate.innerText = `${key}: ${ratesText}`;
+                $newRate.classList.add('m-4', 'fs-6', 'lh-sm', 'fw-light');
+                $ratesContainer.appendChild($newRate);
+            });
+    
+            $data.appendChild($ratesContainer);
+            $data.style.display = '';
+        })
+        .catch(error => console.error("FAILED", error));
+    }
+
+    
+}
+
 const $submitButton = document.querySelector('#submit-button');
 const $from = document.querySelector('#from');
 const $to = document.querySelector('#to');
@@ -19,7 +122,7 @@ const currentTime = new Date().toISOString().split("T")[0];
 $startDate.max = currentTime;
 $endDate.max = currentTime;
 
-document.querySelector('#submit-button').onclick = getData;
+document.querySelector('#submit-button').onclick = getExchangeData;
 
 function loadSymbols () {
     fetch('https://api.frankfurter.app/currencies')
@@ -45,79 +148,19 @@ function loadSymbols () {
 }
 
 
-function getData() {
-    const $data = document.querySelector('#data');
+function getExchangeData() {
     cleanErrors();
 
     if ( !($amount.value > 0 && /^[0-9]+$/.test($amount.value)) ){
         createError($amount, '"Amount" must be a number greater than 0');
     }
-    if ($from.value === $to.value){
-        $to.value = '';
-    }
 
     if(document.querySelectorAll('.error').length > 0){
         return;
     }
-    fetch(getFetch())
-    .then(response => response.json())
-    .then(response => {
-        let dataText = `Exchange Rates for ${$amount.value} ${$from.value} `;
 
-            dataText = dataText + `to ${$to.value} `;
-        }
-
-        dataText = dataText + `on `
-
-        let dataDate;
-
-            dataDate = `${$startDate.value}`;
-                dataDate = dataDate + ` - ${$endDate.value}`;
-
-                if ($startDate.value === $endDate.value){
-                    dataDate = `${$endDate.value}`;
-                }
-            }
-            else {
-                dataDate = dataDate + ` - ${currentTime}`;
-            }
-        }
-        else {
-                dataDate = `${$endDate.value}`;
-            }
-            else {
-                dataDate = `${currentTime}`;
-            }
-        }
-
-        dataText = dataText + dataDate;
-        
-        $data.innerText = dataText;
-        $data.classList.add('fs-4', 'fw-bold');
-
-        const $ratesContainer = document.createElement('span');
-        $ratesContainer.id = 'rates';
-
-        Object.entries(response['rates']).forEach(([key, values]) =>{
-            const $newRate = document.createElement('li');
-            let ratesText;
-
-            if (Object.entries(values).length > 0){
-                ratesText = Object.entries(values).map(([currency, value]) => `\n${currency}: ${value}`).join('');
-            }
-            else{
-                ratesText = values;
-            }
-
-            $newRate.innerText = `${key}: ${ratesText}`;
-            $newRate.classList.add('m-4', 'fs-6', 'lh-sm', 'fw-light');
-            $ratesContainer.appendChild($newRate);
-        });
-
-        $data.appendChild($ratesContainer);
-        $data.style.display = '';
-    })
-    .catch(error => console.error("FAILED", error));
+    const exchange = new Exchange($from.value, $to.value, $amount.value, $startDate.value, $endDate.value);
+    exchange.getRates();
 }
 
 
@@ -136,28 +179,3 @@ function createError(element, errorMessage){
     $newItem.innerText = errorMessage;
     $errors.appendChild($newItem);
 }
-
-
-function getFetch() {
-    let URL = `https://api.frankfurter.app/`;
-
-        URL = URL + `${$startDate.value}..`;
-    }
-
-        URL = URL + `${$endDate.value}`;
-    }
-
-        URL = URL + `latest`;
-    }
-
-    URL = URL + `?from=${$from.value}`;
-
-        URL = URL + `&to=${$to.value}`;
-    }
-
-    URL = URL + `&amount=${$amount.value}`;
-
-    return URL;
-
-}
-
